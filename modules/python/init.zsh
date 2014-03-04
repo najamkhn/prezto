c1857bc583aa18107cc4614304ce75d41a76b0d2
Najam Khan <najamkhn@gmail.com>
2014-03-04 14:22:29 +0400

    custom: enable custom modules

--- modules/python/init.zsh
+++ modules/python/init.zsh
@@ -37,7 +37,7 @@ if (( $+commands[virtualenvwrapper_lazy.sh] )); then
   export WORKON_HOME="$HOME/.virtualenvs"
 
   # Disable the virtualenv prompt.
-  VIRTUAL_ENV_DISABLE_PROMPT=1
+  # VIRTUAL_ENV_DISABLE_PROMPT=0
 
   source "$commands[virtualenvwrapper_lazy.sh]"
 fi
